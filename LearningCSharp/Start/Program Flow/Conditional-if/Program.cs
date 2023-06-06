3b0c183cd870e13db4a0e970f0fde46b12b323b8
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-06 14:50:53 +0100

    Program Flow chapter completed

--- LearningCSharp/Start/Program Flow/Conditional-if/Program.cs
+++ LearningCSharp/Start/Program Flow/Conditional-if/Program.cs
@@ -9,7 +9,18 @@ namespace Conditionals
             int theVal = 50;
 
             // TODO: if-else 
-
+            if (theVal == 50)
+            {
+                System.Console.WriteLine("theVal is 50");
+            }
+            else if (theVal >= 51 && theVal <= 60)
+            {
+                System.Console.WriteLine("theVal is between 51 and 60");
+            }
+            else
+            {
+                System.Console.WriteLine("theVal is something else");
+            }
 
             // -----------------------
             // TODO: Using the ternary operator ?:
@@ -25,6 +36,8 @@ namespace Conditionals
             */
 
             // TODO: can be replaced by a ternary operator ?:
+
+            System.Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
 
         }
     }
--- LearningCSharp/Start/Program Flow/Exceptions/Program.cs
+++ LearningCSharp/Start/Program Flow/Exceptions/Program.cs
@@ -6,13 +6,35 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            int x = 100;
-            int y = 10;
+            int x = 1005;
+            int y = 0;
             int result;
 
             // TODO: try-catch expressions make error checking easier
-            result = x / y;
-            Console.WriteLine("The result is: {0}", result);
+            try
+            {
+                if (x > 1000)
+                {
+                    throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                System.Console.WriteLine("Oh no!!");
+                System.Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                System.Console.WriteLine("Sorry, 1000 is the limit");
+                System.Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                System.Console.WriteLine("This always runs!");
+            }
         }
     }
 }
