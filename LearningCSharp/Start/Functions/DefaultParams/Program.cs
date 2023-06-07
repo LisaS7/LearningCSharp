5946c82ebf8ac86e97f1bc228f3866087fc3953d
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-07 12:00:29 +0100

    Completed Functions chapter

--- LearningCSharp/Start/Functions/DefaultParams/Program.cs
+++ LearningCSharp/Start/Functions/DefaultParams/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 
 // TODO: Functions can provide default values for their parameters
-void PrintWithPrefix(string thestr, string prefix)
+void PrintWithPrefix(string thestr, string prefix = "")
 {
     Console.WriteLine($"{prefix} {thestr}");
 }
 
 // TODO: Test the default parameters
-
+PrintWithPrefix("Hey dude");
+PrintWithPrefix("Hey dude", "__ >");
 
 // TODO: Call with named params
-
+PrintWithPrefix(prefix: "_-_-_ ", thestr: "Hey dude");
---
+++ LearningCSharp/Start/Functions/ProgChallenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+// function to reverse string
+static string ReverseString(string inputStr)
+{
+    char[] charArray = inputStr.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+// function isPalindrome, returns bool and int
+static (bool, int) IsPalindrome(string inputStr)
+{
+    inputStr = inputStr.ToLower();
+    var sb = new StringBuilder();
+
+    foreach (char c in inputStr)
+    {
+        if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+        {
+            sb.Append(c);
+        }
+    }
+
+    string formattedString = sb.ToString();
+    string reversedString = ReverseString(formattedString);
+
+    return formattedString == reversedString ? (true, formattedString.Length) : (false, 0);
+
+}
+
+string userInput = null;
+while (userInput != "exit")
+{
+    System.Console.WriteLine("Enter text: ");
+    userInput = Console.ReadLine();
+    (bool check, int length) result = IsPalindrome(userInput);
+    System.Console.WriteLine($"Is it a palindrome? {result.check}. Length: {result.length}");
+}
