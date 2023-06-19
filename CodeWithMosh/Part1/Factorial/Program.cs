faa66ab6c87579738b80c3b31696dfe621209e78
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-19 16:12:51 +0100

    Factorial exercise

---
+++ CodeWithMosh/Part1/Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Choose a number: ");
+int number = Int32.Parse(Console.ReadLine());
+
+int factorial = 1;
+int current = number;
+while (current > 0)
+{
+    factorial *= current;
+    current--;
+}
+
+Console.WriteLine($"{number}! = {factorial}");
