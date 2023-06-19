c94bc9a37f3ba80291b19db67a394bd0a8c653f1
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-19 16:20:46 +0100

    Deleted unnecessary files

--- EssentialTraining/CH02/01/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using LinkedIn.Essentials;
-
-Employee e = new Employee("Alec", "Steven");
-System.Console.WriteLine($"{e.FirstName} says hello!");
--- EssentialTraining/CH02/03/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using LinkedIn.Essentials;
-
-Employee e = new Employee("Spock", "Steven", 27)
-{
-    StartDate = new DateOnly(2023, 06, 03)
-};
-
-Console.WriteLine($"From {e.FirstName}, hello world");
