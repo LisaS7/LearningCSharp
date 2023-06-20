c4aab06041e781655dde3fe3f0cb7f78e0c61696
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-20 15:20:54 +0100

    StringBuilder lesson complete

---
+++ CodeWithMosh/Part1/Strings/StringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = new StringBuilder("0000000");
+        builder.Append('-', 10)
+        .AppendLine()
+        .Append("Header")
+        .AppendLine()
+        .Append('-', 10)
+
+        .Replace('-', '=')
+        .Remove(0, 5)
+        .Insert(0, new string('-', 5));
+        Console.WriteLine(builder);
+    }
+}
