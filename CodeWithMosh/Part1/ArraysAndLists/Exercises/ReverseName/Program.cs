d890108825b8c7fa1b59bb0b93f542185475c458
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-20 12:23:17 +0100

    Reverse name exercise complete

---
+++ CodeWithMosh/Part1/ArraysAndLists/Exercises/ReverseName/Program.cs
@@ -0,0 +1,19 @@
+﻿// Write a program and ask the user to enter their name. 
+// Use an array to reverse the name and then store the result in a new string. 
+// Display the reversed name on the console.
+
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+var letters = new char[name.Length];
+
+for (int i = 0; i < name.Length; i++)
+{
+    letters[i] = name[i];
+}
+Array.Reverse(letters);
+
+string reversedName = string.Join("", letters);
+
+Console.WriteLine("Your name backwards is " + reversedName);
+
+
