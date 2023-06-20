3de9443f7186ebb8924dbb819ee37d2781bd4764
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-20 12:33:08 +0100

    Unique numbers exercise complete

---
+++ CodeWithMosh/Part1/ArraysAndLists/Exercises/UniqueNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Write a program and ask the user to enter 5 numbers. 
+//  If a number has been previously entered, display an error message and ask the user to re-try. 
+//  Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+
+var numbers = new List<int>();
+
+
+while (numbers.Count < 5)
+{
+    Console.WriteLine("Please enter a number: ");
+    int number = Int32.Parse(Console.ReadLine());
+    int index = numbers.IndexOf(number);
+
+    if (index == -1)
+    {
+        numbers.Add(number);
+    }
+    else
+    {
+        Console.WriteLine("Sorry, that number has already been entered!");
+    }
+
+}
+
+numbers.Sort();
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+}
