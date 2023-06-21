f3a827d10f92e5ad62d499d7811a0d90241f85c0
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-21 13:10:36 +0100

    Files lesson complete

---
+++ CodeWithMosh/Part1/Files/File/Program.cs
@@ -0,0 +1,15 @@
+﻿string pwd = @"/Users/lisa/Documents/LearningCSharp/CodeWithMosh/Part1/Files/File/";
+
+File.Copy(pwd + "Text.txt", pwd + "Text2.txt", true);
+File.Copy(pwd + "Text2.txt", pwd + "Text3.txt", true);
+File.Delete(pwd + "Text2.txt");
+
+if (File.Exists(pwd + "Text3.txt")) {
+    Console.WriteLine("File copied successfully!");
+}
+
+string content = File.ReadAllText(pwd + "Text3.txt");
+Console.WriteLine(content);
+
+var fileInfo = new FileInfo(pwd + "Text.txt");
+fileInfo.CopyTo(pwd + "file_info.txt");
