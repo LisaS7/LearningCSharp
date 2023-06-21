ddcd7f96593d9db70261d5274dde882efcc75f3d
Lisa <84908214+LisaS7@users.noreply.github.com>
2023-06-21 13:31:34 +0100

    Directory lesson complete

--- CodeWithMosh/Part1/Files/Directory/Program.cs
+++ CodeWithMosh/Part1/Files/Directory/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string pwd = @"/Users/lisa/Documents/LearningCSharp/CodeWithMosh/Part1/Files/Directory/";
+string testDirectory = @"/Users/lisa/Documents/LearningCSharp/CodeWithMosh";
+
+Directory.CreateDirectory(pwd + "ILikeCheese");
+
+var files = Directory.GetFiles(testDirectory, "*.txt", SearchOption.AllDirectories);
+foreach(string file in files)
+    Console.WriteLine(file);
+
+
+Console.WriteLine();
+
+var directories = Directory.GetDirectories(testDirectory, "*.*", SearchOption.AllDirectories);
+foreach (string dir in directories)
+{
+    Console.WriteLine(dir);
+}
+
+Console.WriteLine();
+
+bool hasDocFolder = Directory.Exists(@"/Users/lisa/Documents");
+Console.WriteLine(hasDocFolder);
+
+var directoryInfo = new DirectoryInfo(pwd);
+directoryInfo.GetFiles();
+directoryInfo.GetDirectories();
+
+
 
